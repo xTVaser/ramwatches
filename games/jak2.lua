8451de8e6c26e0b44af482a3cacd00a9faa97c79
Tyler Wilding <xtvaser@gmail.com>
2017-05-10 23:49:18 -0400

    Headers for layout added

--- games/jak2.lua
+++ games/jak2.lua
@@ -7,10 +7,9 @@ local gameModule = require 'game'
 package.loaded.valuetypes = nil
 local valuetypes = require 'valuetypes'
 
-
 local Jak2 = subclass(gameModule.Game)
 
-Jak2.layoutModuleNames = {'custom_layouts'}
+Jak2.layoutModuleNames = {'jak2_layout'}
 Jak2.framerate = 60
 
 function Jak2:init(options)
@@ -26,37 +25,30 @@ end
 local StaticValue = subclass(valuetypes.MemoryValue)
 function StaticValue:getAddress()
 	--negate the offset because we dont need it
-  return self.game.startAddress - self.game.startAddress 
+	return self.game.startAddress - self.game.startAddress 
 end
 
-
 -- Position.
--- short hand way but i cant get it to work with constant addresses
---Jak2.blockValues.posX = valuetypes.MV("Pos X", 0x20197790, StaticValue, valuetypes.FloatTypeLE)
----Jak2.blockValues.posY = valuetypes.MV("Pos Y", 0x46B9CC, 0x0, valuetypes.FloatType)
---Jak2.blockValues.posZ = valuetypes.MV("Pos Z", 0x46B9DC, 0x0, valuetypes.FloatType)
-
-
 function Jak2:xPositionDisplay()
-  local xPositionAddress = 0x20197790
-  local xPosition = utils.readFloatLE(xPositionAddress)
-  -- Find the change in X position using the previous position with the current one
-  -- Absolute value to calculate only magnitude not direction
-  deltaX = math.abs(xPosition) - math.abs(prevXPos)
-  -- Update the position
-  prevXPos = xPosition
-  return "X position:\t\t" .. utils.floatToStr(xPosition) .. " u/f"
+	local xPositionAddress = 0x20197790
+	local xPosition = utils.readFloatLE(xPositionAddress)
+	-- Find the change in X position using the previous position with the current one
+	-- Absolute value to calculate only magnitude not direction
+	deltaX = math.abs(xPosition) - math.abs(prevXPos)
+	-- Update the position
+	prevXPos = xPosition
+	return "X position:\t\t" .. utils.floatToStr(xPosition) .. " u/f"
 end
 
 function Jak2:zPositionDisplay()
-  local zPositionAddress = 0x20197798
-  local zPosition = utils.readFloatLE(zPositionAddress)
-  -- Find the change in X position using the previous position with the current one
-  -- Absolute value to calculate only magnitude not direction
-  deltaZ = math.abs(zPosition) - math.abs(prevZPos)
-  -- Update the position
-  prevZPos = zPosition
-  return "Z position:\t\t" .. utils.floatToStr(zPosition) .. " u/f"
+	local zPositionAddress = 0x20197798
+	local zPosition = utils.readFloatLE(zPositionAddress)
+	-- Find the change in X position using the previous position with the current one
+	-- Absolute value to calculate only magnitude not direction
+	deltaZ = math.abs(zPosition) - math.abs(prevZPos)
+	-- Update the position
+	prevZPos = zPosition
+	return "Z position:\t\t" .. utils.floatToStr(zPosition) .. " u/f"
 end
 
 deltaX = 0
@@ -69,15 +61,15 @@ function Jak2:horizontalSpeed()
 	-- sqrt((x2-x1)^2 + (y2-y1)^2) 
 	speed = math.sqrt((deltaX^2) + (deltaZ^2))
   
-  return "Horizontal Speed:\t" .. utils.floatToStr(speed) .. " u/f"
+	return "Horizontal Speed:\t" .. utils.floatToStr(speed) .. " u/f"
 end
 
 function Jak2:checkpointAddr()
-  
-  local checkpointAddress = 0x20622FA0
-  local checkpoint = utils.readIntLE(checkpointAddress, 4)
-  
-  return "Checkpoint Address:\t" .. utils.intToStr(checkpoint)
+
+	local checkpointAddress = 0x20622FA0
+	local checkpoint = utils.readIntLE(checkpointAddress, 4)
+
+	return "Checkpoint Address:\t" .. utils.intToStr(checkpoint)
 end
 
 -- Hack that isnt nessecary?
--- layouts/jak2_layout.lua
+++ layouts/jak2_layout.lua
@@ -7,31 +7,51 @@ local layoutsModule = require 'layouts'
 local Layout = layoutsModule.Layout
 
 local margin = 6
-local fontSize = 12
-local fixedWidthFontName = "Consolas"
+local fontSize = 10
+local fontSizeHeader = 12
+local contentFontName = "Consolas"
+local headerFontName = "Arial"
+local headerFontColor = 0x000000
 
 local layouts = {}
-
 
 layouts.Jak2Layout = subclass(Layout)
 function layouts.Jak2Layout:init()
 
-self.margin = margin
-  self:setUpdatesPerSecond(5)
-  self.window:setSize(400, 300)
+	self.margin = margin
+	self:setUpdatesPerSecond(5)
+	self.window:setSize(400, 800)
 
-  
+	self:addLabel{fontSize=fontSizeHeader, fontName=headerFontName, fontColor=headerFontColor}
+	self:addItem("Jak Status")
+	
+	self:addLabel{fontSize=fontSizeHeader, fontName=headerFontName, fontColor=headerFontColor}
+	self:addItem("Position and Movement")
+	
+	self:addLabel{fontSize=fontSizeHeader, fontName=headerFontName, fontColor=headerFontColor}
+	self:addItem("Game Info")
+	
+	self:addLabel{fontSize=fontSizeHeader, fontName=headerFontName, fontColor=headerFontColor}
+	self:addItem("Inventory")
+	
+	self:addLabel{fontSize=fontSizeHeader, fontName=headerFontName, fontColor=headerFontColor}
+	self:addItem("Objectives")
+	
+	self:addLabel{fontSize=fontSizeHeader, fontName=headerFontName, fontColor=headerFontColor}
+	self:addItem("Boss Information")
+	
+	self:addLabel{fontSize=fontSizeHeader, fontName=headerFontName, fontColor=headerFontColor}
+	self:addItem("Secrets")
 
-  self:addLabel{fontSize=fontSize, fontName=fixedWidthFontName}
-  self:addItem(self.game.xPositionDisplay)
-  self:addItem(self.game.zPositionDisplay)
-  self:addItem(self.game.horizontalSpeed)
-  self:addItem(self.game.checkpointAddr)
-  
+	self:addLabel{fontSize=fontSize, fontName=contentFontName}
+	self:addItem(self.game.xPositionDisplay)
+	self:addItem(self.game.zPositionDisplay)
+	self:addItem(self.game.horizontalSpeed)
+	self:addItem(self.game.checkpointAddr)
+
 	self:activateAutoPositioningY()
 end
 
-
 return {
-  layouts = layouts,
+	layouts = layouts,
 }
